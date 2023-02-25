3c066f3a3577331a310fd1399ff087c656382948
Henry P B <mahe2di@gamil.com>
2023-02-24 22:15:04 -0500

    cambio de Idioma

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function seleccionar() {
   document.getElementById("nav").classList = "";
   menuVisible = false;
 }
+
+var check = document.querySelector(".check");
+check.addEventListener("click", idioma);
+
+function idioma() {
+  let id = check.checked;
+  if (id == true) {
+    location.href = "indexSp.html";
+  } else {
+    location.href = "index.html";
+  }
+}
