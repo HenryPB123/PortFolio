8745eeedbe52254323195164049d2e16ac99e3e0
Henry P B <mahe2di@gmail.com>
2025-01-14 21:14:28 -0500

    Updating portfolio

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ function idioma() {
     location.href = "index.html";
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const imageRow = document.querySelector(".image-row");
+  const imageFolder = "blogImages";
+  const imageCount = 8; // Number of images in the folder
+
+  for (let i = 1; i <= imageCount; i++) {
+    const img = document.createElement("img");
+    img.src = `${imageFolder}img${i}.jpg`; // Images named as image1.jpg, image2.jpg, etc.
+    img.alt = `Image ${i}`;
+    imageRow.appendChild(img);
+  }
+});
